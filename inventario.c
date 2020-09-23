1f051158dd2853b201796e2c9816cade9e5ea679
Wicho <luiscalderonmiranda@gmail.com>
2020-09-22 21:35:19 -0500

    First commit

---
+++ inventario.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main(){
+	int option = 0;
+	printf("Proyecto Investigacion de operaciones II");
+	printf("\n\nModelos de Inventario\n\nPor favor Seleccione la opcion que desee");
+	printf("\n\n1.Modelo de compra SIN deficit\n2.Modelo de compra CON deficit\n");
+	scanf("%d", &option);
+	
+}
