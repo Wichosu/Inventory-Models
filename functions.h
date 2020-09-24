ce335e7d941c98978e1d8d8976e30512645cd7e2
Wicho <luiscalderonmiranda@gmail.com>
2020-09-23 20:16:55 -0500

    Basic solution for first model, purchase model without deficit

--- functions.h
+++ functions.h
@@ -1,1 +1,24 @@
-functions
+/*ENG: Aditional file only for functions to be used in inventario.c
+ ESP: Archivo adicional con funciones para usarse en el archivo inventario.c*/
+#include <stdio.h>
+#include <math.h>
+/*ENG: Receives a value and check for the correct message to be display
+  ESP: Recibe un valor y revisa que el mensaje correcta se muestre en pantalla
+void messageForChosenOption(int option){
+	switch(option){
+		case 1: printf("\nHas seleccionado modelo de compra SIN deficit\n");
+				break;
+		case 2: printf("\nHas seleccionado modelo de compra CON deficit\n");
+				break;
+		default:printf("\nSaliendo...\n");
+				break;
+	}
+}*/
+
+void calculateData(double demand, double orderingCost, double maintenanceCost){
+	double totalOrderingCost = 0, totalMaintenanceCost = 0, totalCost = 0, optimalQuantity = 0;
+	optimalQuantity = sqrt(((2*demand*orderingCost)/maintenanceCost));
+	totalOrderingCost = (demand/optimalQuantity) * orderingCost;
+	totalMaintenanceCost = (optimalQuantity/2) * maintenanceCost;
+	totalCost = totalOrderingCost + totalMaintenanceCost;
+}
--- inventario.c
+++ inventario.c
@@ -1,10 +1,38 @@
 #include <stdio.h>
+#include <math.h>
+
 
 int main(){
 	int option = 0;
-	printf("Proyecto Investigacion de operaciones II");
-	printf("\n\nModelos de Inventario\n\nPor favor Seleccione la opcion que desee");
-	printf("\n\n1.Modelo de compra SIN deficit\n2.Modelo de compra CON deficit\n");
-	scanf("%d", &option);
-	
+	double demand = 0, orderingCost = 0, maintenanceCost = 0, totalOrderingCost = 0, totalMaintenanceCost = 0, totalCost = 0, optimalQuantity = 0;
+	do{
+		printf("Proyecto Investigacion de operaciones II");
+		printf("\n\nModelos de Inventario\n\nPor favor Seleccione la opcion que desee");
+		printf("\n\n1.Modelo de compra SIN deficit\n2.Modelo de compra CON deficit\n\nIngrese cualquier otro numero para salir\n");
+		scanf("%d", &option);
+		switch(option){
+			case 1: printf("\nHas seleccionado modelo de compra SIN deficit\n\nPor favor ingrese los siguentes datos:\n\n");
+					printf("Demanda: ");
+					scanf("%lf", &demand);
+					printf("\n\nCosto por ordenar el pedido: ");
+					scanf("%lf", &orderingCost);
+					printf("\n\nCosto de mantener una unidad: ");
+					scanf("%lf", &maintenanceCost);
+					//good at this point
+					optimalQuantity = sqrt(((2*demand*orderingCost)/maintenanceCost));
+					totalOrderingCost = (demand/optimalQuantity) * orderingCost;
+					totalMaintenanceCost = (optimalQuantity/2) * maintenanceCost;
+					totalCost = totalOrderingCost + totalMaintenanceCost;
+					printf("\n\nLa cantidad optima es de: %.2lf unidades por pedido", optimalQuantity);
+					printf("\n\nEl costo total por ordenar un pedido es de: $%.2lf por pedido", totalOrderingCost);
+					printf("\n\nEl costo total de mantenimiento es de: $%.2lf por unidad de tiempo\n\n", totalMaintenanceCost);
+					break;
+			case 2: printf("\nHas seleccionado modelo de compra CON deficit\n");
+					break;
+			default:printf("\nSaliendo...\n");
+					break;
+		}
+	}
+	while(option >= 1 && option <= 2);
+	return 0;
 }
